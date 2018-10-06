0d00ca4256b7bd9b9e0cdb90e7263350fdd81f8a
TerryZ <terry5@foxmail.com>
2018-10-06 16:40:18 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import region from './Region.vue';
 
 const Plugin = {
-    install(Vue){
+    install(Vue, options = {}){
+        if(Object.keys(options).length){
+            if(typeof options.i18n === 'string') region.props.i18n.default = options.i18n;
+            if(typeof options.blank === 'boolean') region.props.blank.default = options.blank;
+            if(typeof options.town === 'boolean') region.props.town.default = options.town;
+            if(typeof options.search === 'boolean') region.props.search.default = options.search;
+        }
+
         Vue.component(region.name, region);
     }
 };
