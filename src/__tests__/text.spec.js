d6138de4f59d989025e240377fd946308d0e90b7
TerryZ <terry5@foxmail.com>
2024-07-10 15:07:16 +0800

    test: RegionText 数据变更响应

--- src/__tests__/text.spec.js
+++ src/__tests__/text.spec.js
@@ -4,7 +4,7 @@ import { mount } from '@vue/test-utils'
 import { RegionText } from '@/'
 
 describe('v-region Text 纯文本模式', () => {
-  test('使用数据初始化插件后，输出的内容应为："北京市北京市东城区东华门街道"', async () => {
+  describe('初始化与变更', async () => {
     const wrapper = mount(RegionText, {
       props: {
         modelValue: {
@@ -16,8 +16,24 @@ describe('v-region Text 纯文本模式', () => {
       }
     })
     await vi.dynamicImportSettled()
-    expect(wrapper.text()).to.equal('北京市北京市东城区东华门街道')
+
+    test('使用数据初始化后，输出的内容应为：`北京市北京市东城区东华门街道`', async () => {
+      expect(wrapper.text()).to.equal('北京市北京市东城区东华门街道')
+    })
+    test('修改数据后，输出内容应为 `福建省福州市仓山区金山街道`', async () => {
+      await wrapper.setProps({
+        modelValue: {
+          province: '350000',
+          city: '350100',
+          area: '350104',
+          town: '350104008'
+        }
+      })
+      await vi.dynamicImportSettled()
+      expect(wrapper.text()).to.equal('福建省福州市仓山区金山街道')
+    })
   })
+
   test('"separator" prop 设置为 "-"，输出的内容应为："北京市-北京市-东城区-东华门街道"', async () => {
     const wrapper = mount(RegionText, {
       props: {
