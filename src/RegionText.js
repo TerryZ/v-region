c53379d8043b14d269ff25aed94e6a519521acf0
Terry <terry5@foxmail.com>
2024-09-11 22:17:10 +0800

    feat: text 组件应用核心模块

--- src/RegionText.js
+++ src/RegionText.js
@@ -1,23 +1,14 @@
-import { h, ref, defineComponent, watchEffect } from 'vue'
-import { validModel } from './utils/helper'
-import { valueToModel, regionToText } from './utils/parse'
+import { h, defineComponent } from 'vue'
+import { useRegion, mergeBaseProps } from './core/base'
 
 export default defineComponent({
   name: 'RegionText',
-  props: {
-    modelValue: { type: Object, default: undefined },
+  props: mergeBaseProps({
     separator: { type: String, default: '' }
-  },
+  }),
   setup (props) {
-    const text = ref('')
+    const { regionText } = useRegion(props)
 
-    watchEffect(() => {
-      if (!validModel(props.modelValue)) return
-      valueToModel(props.modelValue).then(resp => {
-        text.value = regionToText(resp).join(props.separator)
-      })
-    })
-
-    return () => h('span', text.value)
+    return () => h('span', regionText.value)
   }
 })
