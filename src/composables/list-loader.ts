dd07cd173a7cb96b4904cc8de875d9601e2a265b
TerryZeng <terry5@foxmail.com>
2026-08-26 02:46:23 +0800

    feat: 使用编码前后缀匹配的方式过滤列表

--- src/composables/list-loader.ts
+++ src/composables/list-loader.ts
@@ -12,33 +12,39 @@ import type { RegionItem } from '../types'
 export function getCities(province?: RegionItem): RegionItem[] {
   if (!province || !Object.keys(province).length) return []
 
-  const code = Number(province.key)
-  const list = regionCities.filter((val) => {
-    const current = Number(val.key)
-    return current - code < 1e4 && current % code < 1e4
-  })
+  // const code = Number(province.key)
+  // const list = regionCities.filter((val) => {
+  //   const current = Number(val.key)
+  //   return current - code < 1e4 && current % code < 1e4
+  // })
+  const prefix = province.key!.substring(0, 2)
+  const list = regionCities.filter((city) => city.key?.startsWith(prefix))
   // 城市/直辖市
   return list.length ? list : [province]
 }
 /**
  * 根据城市读取区/县列表
  *
- * @param cityItem - 城市
+ * @param city - 城市
  * @returns 区/县列表
  */
-export function getAreas(cityItem?: RegionItem): RegionItem[] {
-  if (!cityItem) return []
+export function getAreas(city?: RegionItem): RegionItem[] {
+  if (!city || !Object.keys(city).length) return []
 
-  const city = Number(cityItem.key)
+  // const cityCode = Number(city.key)
   // 城市是否为直辖市/特别行政区
-  const isNotProvince = city % 1e4
-  const calcNum = isNotProvince ? 100 : 1e4
-  const list = regionAreas.filter((val) => {
-    const areaCode = Number(val.key)
-    return areaCode - city < calcNum && areaCode % city < calcNum
-  })
+  // const isNotRegular = cityCode % 1e4
+  const isNotRegular = city.key?.endsWith('0000')
+  // const calcNum = isNotRegular ? 100 : 1e4
+  const prefixLength = isNotRegular ? 2 : 4
+  const prefix = city.key?.substring(0, prefixLength)
+  // const list = regionAreas.filter((val) => {
+  //   const areaCode = Number(val.key)
+  //   return areaCode - cityCode < calcNum && areaCode % cityCode < calcNum
+  // })
+  const list = regionAreas.filter((area) => area.key?.startsWith(prefix!))
   // 区县/地级市
-  return list.length ? list : [cityItem]
+  return list.length ? list : [city]
 }
 /**
  * 根据区/县数据读取乡/镇列表
